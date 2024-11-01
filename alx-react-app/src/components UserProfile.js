a54aa8f9005a1f1a8b523553e68decc53b1a8ebe
Yohannes Atirsaw <yjohn4013@gmail.com>
2024-10-31 21:07:50 -1200

    src/components UserProfile.js

---
+++ alx-react-app/src/components UserProfile.js
@@ -0,0 +1,9 @@
+const UserProfile = (props) => {
+   return (
+     <div>
+       <h2>{props.Yohannes}</h2>
+       <p>Age: {props.21}</p>
+       <p>Bio: {props.I am john}</p>
+     </div>
+   );
+ };
