0468f4cc1036084b8166a646a68dd45414f12394
Yohannes Atirsaw <yjohn4013@gmail.com>
2024-11-03 04:18:33 -1200

    Update and rename  UserProfile to  UserProfile.jsx

--- alx-react-app/ UserProfile.jsx
+++ alx-react-app/ UserProfile.jsx
@@ -1,6 +1,10 @@
+      const Greeting = (props) => {
+  return <h1>Hello, {props.Yohannes}</h1>;
+};
 function UserProfile() {
 const UserProfile = (props) => {
    return (
+
      <div>
        <h2>{props.Yohannes}</h2>
        <p>Age: {props.21}</p>
