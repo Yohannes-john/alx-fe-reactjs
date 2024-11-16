244cacb348c2d07202b1185f0485e664f5d54e69
Yohannes Atirsaw <yjohn4013@gmail.com>
2024-11-16 18:02:59 +0300

    Create Footer.jsx

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{
+      backgroundColor: '#333',
+      color: '#fff',
+      padding: '20px',
+      textAlign: 'center'   
+
+    }}>
+      <p>&copy; 2023 My Website</p>
+    </footer>
+  );
+}
+
+export default Footer;
