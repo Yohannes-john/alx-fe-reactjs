2d3ebcb7004502b2b6ad8d98eb0a0dc2fcdf046c
Yohannes Atirsaw <yjohn4013@gmail.com>
2024-11-16 17:47:44 +0300

    Create MainContent.jsx

---
+++ alx-react-app-new/MainContent.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react';
+
+function MainContent() {
+  return (
+    <div style={{
+      backgroundColor: '#f2f2f2',
+      padding: '20px',
+      margin: '20px',
+      borderRadius: '10px',
+      boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.1)'
+    }}>
+      {/* Content goes here */}
+      <h1>Main Content</h1>
+      <p>This is the main content area.</p>
+    </div>
+  );
+}
+
+export default MainContent;
