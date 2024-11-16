6301ce97fc451e3cc8eabe196eca5f5f43383288
Yohannes Atirsaw <yjohn4013@gmail.com>
2024-11-16 22:16:27 +0300

    Create Navbar.jsx

---
+++ my-company/Navbar.jsx
@@ -0,0 +1,30 @@
+
+:
+
+JavaScript
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>   
+
+        </li>
+        <li>
+          <Link to="/services">Services</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
