b754065214694b1444b41474dbfa39c50b606145
Yohannes Atirsaw <yjohn4013@gmail.com>
2024-11-16 20:03:00 +0300

    Create ProfilePage.jsx

---
+++ alx-react-app-props/ProfilePage.jsx
@@ -0,0 +1,14 @@
+/ ProfilePage.jsx
+import React from 'react';
+import UserDetails from './UserDetails';
+
+function ProfilePage() {
+  return (
+    <div>
+      <h2>Profile</h2>
+      <UserDetails />
+    </div>
+  );
+}
+
+export default ProfilePage;
