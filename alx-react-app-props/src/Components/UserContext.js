5cc967494dc31cddd09cf1e3a52ef9a58dcefb1e
Yohannes Atirsaw <yjohn4013@gmail.com>
2024-11-17 19:20:38 +0300

    Update UserContext.js

--- alx-react-app-props/src/Components/UserContext.js
+++ alx-react-app-props/src/Components/UserContext.js
@@ -1,35 +1,4 @@
 
-// UserContext.js
- import React.createContext()
-import React, { createContext, useState, useEffect } from 'react';
-
-// Create a Context object
-const UserContext = createContext();
-
-// Create a UserProvider component
-const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(null);   
-
-
-  // Fetch user data on component mount (replace with your actual logic)
-  useEffect(() => {
-    const fetchUserData = async () => {
-      const response = await fetch('/api/user');
-      const data = await response.json();
-      setUser(data);
-    };
-
-    fetchUserData();
-  }, []);
-
-  return (
-    <UserContext.Provider value={{ user }}>
-      {children}
-    </UserContext.Provider>
-  );
-};
-
-export { UserContext, UserProvider };
 
 // UserContext.js
 import React, { createContext } from 'react';
