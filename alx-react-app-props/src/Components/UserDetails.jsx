6425a5e362bb55799c8d27600da60ef166cf0998
Yohannes Atirsaw <yjohn4013@gmail.com>
2024-11-17 13:15:21 +0300

    Update UserDetails.jsx

--- alx-react-app-props/src/Components/UserDetails.jsx
+++ alx-react-app-props/src/Components/UserDetails.jsx
@@ -1,4 +1,16 @@
- 
+
+UserDetails.jsx:
+
+function UserDetails({ userData }) {
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserDetails;
 
  
 // UserDetails.jsx
