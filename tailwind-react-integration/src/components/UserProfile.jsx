e11d0cbbf10842caced1aeef64c2d8df84032326
Yohannes Atirsaw <yjohn4013@gmail.com>
2024-12-01 21:35:47 +0300

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -9,7 +9,13 @@ function UserProfile() {
       <p>This is a user profile component.</p>
       {/* ... other profile details ... */}
     </div>
+
+    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+      <img src="user.jpg" alt="User Profile Picture" className="rounded-full w-36 h-36 mx-auto" />
+      {/* ... other profile details ... */}
+    </div>
   );
 }
+
 
 export default UserProfile;
