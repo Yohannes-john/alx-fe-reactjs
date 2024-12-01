43afb54960bbf7518eeefbf011656832ffdb420f
Yohannes Atirsaw <yjohn4013@gmail.com>
2024-12-01 20:30:08 +0300

    Update formikForm.js

--- form-handling-react/formikForm.js
+++ form-handling-react/formikForm.js
@@ -1,1 +1,48 @@
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
+const validationSchema = Yup.object().shape({   
+
+  firstName: Yup.string().required('Required'),
+  lastName: Yup.string().required('Required'),
+  email: Yup.string().email('Invalid email').required('Required'),   
+
+});
+
+const MyForm = () => {
+  const handleSubmit = (values, { setSubmitting }) => {
+    // Handle form submission here
+    console.log(values);
+    setSubmitting(false);
+  };
+
+  return (
+    <Formik
+      initialValues={{   
+
+        firstName: '',
+        lastName: '',
+        email: '',
+      }}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <Field name="firstName" type="text" />
+          <ErrorMessage name="firstName" />
+          <Field name="lastName" type="text" />
+          <ErrorMessage name="lastName" />
+          <Field name="email" type="email" />
+          <ErrorMessage name="email" />
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default MyForm;
