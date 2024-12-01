70b1d73d0ec24c1e06ae5a53b43ad2b79dc9a41f
Yohannes Atirsaw <yjohn4013@gmail.com>
2024-12-01 19:30:42 +0300

    Create RegistrationForm.jsx

---
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+
+function RegistrationForm() {
+    const [userData, setUserData] = useState({
+        name: '',
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (event) => {
+        setUserData({
+            ...userData,
+            [event.target.name]: event.target.value
+        });
+    };
+
+    const handleSubmit   
+ = (event) => {
+        event.preventDefault();
+        console.log(userData);
+        setUserData({
+            name: '',
+            email: '',
+            password:   
+ ''
+        });
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="name">Name:</label>
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={userData.name}
+                    onChange={handleChange}   
+
+                />
+            </div>
+            <div>
+                <label htmlFor="email">Email:</label>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={userData.email}
+                    onChange={handleChange}
+                />
+            </div>   
+            <div>
+                <label htmlFor="password">Password:</label>
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={userData.password}   
+
+                    onChange={handleChange}
+                />
+            </div>
+            <button type="submit">Register</button>   
+
+        </form>
+    );
+}
+
+export default RegistrationForm;
