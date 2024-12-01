495cfa527963d3cd93ab499a624f265b6b2c0a21
Yohannes Atirsaw <yjohn4013@gmail.com>
2024-12-01 21:32:29 +0300

    Update App.jsx

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
+import UserProfile from './UserProfile';
 
+function App() {
+  return (
+    <div>
+      {/* Other components */}
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
