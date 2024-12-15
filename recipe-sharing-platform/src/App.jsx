b29302e8ece6f50e4595a4671998fefe07c3ddc3
Yohannes Atirsaw <yjohn4013@gmail.com>
2024-12-15 20:51:15 +0300

    Update App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,1 +1,18 @@
 
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import RecipeDetail from './components/RecipeDetail';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipes/:id" element={<RecipeDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
