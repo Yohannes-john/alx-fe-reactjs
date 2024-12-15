e2f0b2012cf1c50fee582093bd219817d7b17156
Yohannes Atirsaw <yjohn4013@gmail.com>
2024-12-15 14:39:52 +0300

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
 
+const GITHUB_API_URL = 'https://api.github.com/users/';
+
+const fetchUserData = async (username) => {
+  try {
+    const response = await axios.get(`${GITHUB_API_URL}${username}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching GitHub user data:', error);
+    throw error;
+  }
+};
+
+export default {
+  fetchUserData,
+};
