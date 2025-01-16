1c8eff68f94807bf49e5fb18e3ca4903fa9718d2
Yohannes Atirsaw <yjohn4013@gmail.com>
2025-01-16 14:04:43 +0300

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,3 +2,4 @@ import UserProfile
 import", "from", "WelcomeMessage", "<WelcomeMessage />
 import Header from", "import MainContent from", "import Footer from"
 <Header />", "<Footer />", "<MainContent />
+name=", "Alice", "age=", "25", "bio=", "Loves hiking and photography
