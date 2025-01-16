61562fa3aebfa1e136b45047d02086882ede3856
Yohannes Atirsaw <yjohn4013@gmail.com>
2025-01-16 14:02:01 +0300

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,4 @@
-
+import UserProfile
 import", "from", "WelcomeMessage", "<WelcomeMessage />
 import Header from", "import MainContent from", "import Footer from"
 <Header />", "<Footer />", "<MainContent />
